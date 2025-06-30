4b19f6e496f31d04fa5d1981f77a32065741aa3f
Romain M. <88449922+4strium@users.noreply.github.com>
2025-06-30 19:32:55 +0200

    Updated App with landing page, project creation dialog, etc...

---
+++ modules/newProject.py
@@ -0,0 +1,106 @@
+import os, sys, json
+from PyQt6.QtWidgets import QApplication, QDialog, QLabel, QLineEdit, QPushButton, QMessageBox, QVBoxLayout, QComboBox
+from PyQt6.QtGui import QFont, QPixmap, QFontDatabase
+from PyQt6.QtCore import Qt
+
+class NewProject(QDialog):
+  def __init__(self):
+    super().__init__()
+    self.setModal(True)
+    self.initializeUI()
+
+  def initializeUI(self):
+    self.setFixedSize(450,600)
+    self.setWindowTitle("Nouveau projet...")
+
+    # Load the custom font
+    font_id = QFontDatabase.addApplicationFont("fonts/Huninn.ttf")
+    font_families = QFontDatabase.applicationFontFamilies(font_id)
+
+    if font_families:
+      self.hunnin = font_families[0]
+    else:
+      self.hunnin = "Arial"
+
+    self.setUpWindow()
+
+  def setUpWindow(self):
+    creation_icon = "images/prototype.png"
+
+    try :
+      with open(creation_icon):
+        crea_label = QLabel(self)
+        pixmap = QPixmap(creation_icon)
+        crea_label.setPixmap(pixmap)
+        crea_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    except FileNotFoundError :
+      pass
+
+    name_label = QLabel("Nom de votre projet :", self)
+    name_label.setFont(QFont(self.hunnin, 24))
+    name_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+    self.name_linedit = QLineEdit(self)
+    self.name_linedit.setMaxLength(15)
+    self.name_linedit.setFont(QFont(self.hunnin, 18))
+    self.name_linedit.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+    name_layout = QVBoxLayout()
+    name_layout.addWidget(name_label)
+    name_layout.addWidget(self.name_linedit)
+    name_layout.setSpacing(2)
+
+    size_label = QLabel("Taille de la carte :", self)
+    size_label.setFont(QFont(self.hunnin, 24))
+    size_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    
+    sizes_availables = ["8x8", "16x16", "32x32", "64x64"]
+    self.ddmenu_size = QComboBox()
+    self.ddmenu_size.addItems(sizes_availables)
+    self.ddmenu_size.setFont(QFont(self.hunnin, 18))
+
+    size_layout = QVBoxLayout()
+    size_layout.addWidget(size_label)
+    size_layout.addWidget(self.ddmenu_size)
+    size_layout.setSpacing(2)
+
+    confirm_btn = QPushButton("Valider")
+    confirm_btn.setFont(QFont(self.hunnin, 24))
+    confirm_btn.setMinimumHeight(80)
+    confirm_btn.clicked.connect(self.createDataFile)
+
+    self.creation_layout = QVBoxLayout()
+    self.creation_layout.addWidget(crea_label)
+    self.creation_layout.addStretch(20)
+    self.creation_layout.addLayout(name_layout)
+    self.creation_layout.addStretch(20)
+    self.creation_layout.addLayout(size_layout)
+    self.creation_layout.addStretch(50)
+    self.creation_layout.addWidget(confirm_btn)
+
+    self.setLayout(self.creation_layout)
+
+  def createDataFile(self):
+    if self.name_linedit.text():
+      details = {"name": self.name_linedit.text()}
+
+      map_data = []
+      size_choosen = int((self.ddmenu_size.currentText()).split("x")[0])
+
+      for i in range(size_choosen):
+        line_tmp = []
+        for j in range(size_choosen):
+          line_tmp.append(0)
+        map_data.append(line_tmp)
+      
+      details["map"] = map_data
+      
+      os.makedirs("workingDir", exist_ok=True) 
+      with open("workingDir/data.json", "w") as f:
+        f.write(json.dumps(details))
+
+      self.close()
+    else :
+      QMessageBox.warning(self, "Nom de projet invalide", "Un nom de projet est requis pour la suite du processus.\nMerci de bien vouloir le renseigner.", QMessageBox.StandardButton.Close, QMessageBox.StandardButton.Close)
+      
+
